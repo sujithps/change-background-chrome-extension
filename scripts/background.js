016aff578ae8fc249775299bc282d722116b9c31
Nipun <nipun@kiebot.com>
2019-10-19 11:50:21 +0530

    Nipun | add multiple color picker and reset options

--- scripts/background.js
+++ scripts/background.js
@@ -1,16 +1,28 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ color: '#3aa757' }, function () {
-        console.log("The color is green.");
-    });
+'use strict';
 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([ {
-            conditions: [ new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developer.chrome.com' },
-            })
-            ],
-            actions: [ new chrome.declarativeContent.ShowPageAction() ]
-        } ]);
-    });
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+	if (changeInfo.status === 'complete') {
+		chrome.storage.sync.get("currentColor", function(data) {
+			if (data["currentColor"]) {
+		        chrome.tabs.executeScript(
+		            tabId,
+		            {code: 'document.body.style.backgroundColor = "#' + data["currentColor"] + '";'}
+		        );
+	        }
+		});
+	}
+});
 
+
+chrome.runtime.onInstalled.addListener(function() {
+
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
+				
+			})
+			],
+			actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
+	});
 });
